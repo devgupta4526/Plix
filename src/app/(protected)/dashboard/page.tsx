c280a6dd77afb916735b687d600edf0f2adf2e08
devgupta4526 <devguptamsi@gmail.com>
2025-09-18 00:09:42 +0530

    Added routes and react query

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { onboardUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return (
-    <div>
-      Page
-    </div>
-  )
+type Props = {};
+
+async function Page({}: Props) {
+  const user = await onboardUser();
+
+  if (user.status === 200 || user.status === 201) {
+    return redirect(`/dashboard/${user.data?.firstname}${user.data?.lastname}`);
+  }
+
+  return redirect("/sign-in");
 }
 
-export default page
+export default Page;
